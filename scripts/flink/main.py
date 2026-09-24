46eb178ce1b61f07944322fc2ecb437d0beb0c1d
imash1999 <abduxashimoviman@gmail.com>
2026-09-24 18:58:53 +0800

    fixed flink

--- scripts/flink/main.py
+++ scripts/flink/main.py
@@ -14,7 +14,7 @@ def main():
     
     KAFKA_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka:29092")
     POSTGRES_HOST = os.getenv("POSTGRES_HOST", "postgres")
-    POSTGRES_DB = os.getenv("POSTGRES_DB", "ecommerce_analytics")
+    POSTGRES_DB = os.getenv("POSTGRES_DB", "postgres")
     POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
     POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "root")
 
